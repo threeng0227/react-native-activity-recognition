f648e4dd8e9a23cd8b4319595ee081eb03376c31
Nguyen.Nguyen <threeng0227@gmail.com>
2024-06-04 11:32:17 +0700

    Fix module ios

--- ios/RNActivityRecognition.h
+++ ios/RNActivityRecognition.h
@@ -1,5 +1,12 @@
-#import <React/RCTEventEmitter.h>
+#import <React/RCTBridgeModule.h>
 #import <CoreMotion/CoreMotion.h>
+// Support React Native headers both in the React namespace, where they are in RN version 0.40+,
+// and no namespace, for older versions of React Native
+#if __has_include(<React/RCTEventEmitter.h>)
+#import <React/RCTEventEmitter.h>
+#else
+#import "RCTEventEmitter.h"
+#endif
 
 
 @interface RNActivityRecognition : RCTEventEmitter <RCTBridgeModule>
