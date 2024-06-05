18f82650e0ebf8e4fe7c1e08eaef73e91f28785a
Nguyen.Nguyen <threeng0227@gmail.com>
2024-06-05 16:49:33 +0700

    Revert "Fix module ios"
    This reverts commit f648e4dd8e9a23cd8b4319595ee081eb03376c31.

--- ios/RNActivityRecognition.h
+++ ios/RNActivityRecognition.h
@@ -1,12 +1,5 @@
-#import <React/RCTBridgeModule.h>
-#import <CoreMotion/CoreMotion.h>
-// Support React Native headers both in the React namespace, where they are in RN version 0.40+,
-// and no namespace, for older versions of React Native
-#if __has_include(<React/RCTEventEmitter.h>)
 #import <React/RCTEventEmitter.h>
-#else
-#import "RCTEventEmitter.h"
-#endif
+#import <CoreMotion/CoreMotion.h>
 
 
 @interface RNActivityRecognition : RCTEventEmitter <RCTBridgeModule>
